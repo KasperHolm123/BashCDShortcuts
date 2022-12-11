8c0455783a8f7cbef59711682d9d67bda388896b
Kasper Holm <kasper.marhauer@gmail.com>
2022-12-11 19:08:20 +0100

    better options handling, remove shortcut WIP

--- run.sh
+++ run.sh
@@ -8,9 +8,10 @@ function fill_array() {
     # reset internal container
     shortcuts=()
 
+    # fill internal container with all shortcuts in container dir
     while read line; do 
-        key=$(echo $line | cut -d "|" -f1)
-        data=$(echo $line | cut -d "|" -f2)
+        key=$(echo $line | cut -d "=" -f1)
+        data=$(echo $line | cut -d "=" -f2)
         shortcuts[$key]="$data"
     done < ~/BashCDShortcuts/shortcuts.txt
 }
@@ -22,88 +23,106 @@ then
 fi
 
 function cdproj () {
-    # no options
-    if [ $# == 0 ]
-    then
-        echo "error: no options given"
-        return
-    fi
+    unset opt OPTARG OPTIND # reset environment variables
 
-    # conatiner not found
-    if [ ! -e ~/BashCDShortcuts/shortcuts.txt ] && [ $# == 0 ]
-    then
+    # only allow "-h" if the container dir doesn't exist
+    if [ ! -e ~/BashCDShortcuts ] && [ $1 != "-h" ] && [ $1 != "-m" ]; then
         echo "Container directory missing. Use 'cdproj -m' to create it"
         return
     fi
 
-    # container not found
-    if [ ! -e ~/BashCDShortcuts/shortcuts.txt ] && [ $1 != "-m" ] && [ $1 != "--makedirectory" ] && [ $1 != "-h" ]
-    then
-        echo "Container directory missing. Use 'cdproj -m' to create it"
-        
+    declare cdarg=$1
+    if [ ${cdarg:0:1} != "-" ]; then
+        changedir
         return
     fi
 
-    # help
-    if [ $1 == "-h" ] || [ $1 == "--help" ]
-    then
-        echo "usage"
-        echo "  -l or --list                Shows all available shortcuts"
-        echo "  -m or --makedirectory       Creates shortcut container directory"
-        echo "  -d or --delete              Deletes shortcut container directory"
-        echo "  -c or --changedirectory     Changes directory to the value of a shortcut"
-        echo "  -n or --newshortcut         Creates a new shortcut"
-        echo '      syntax  "KEY|PATH" include double quotes'
-        echo "  -r or --removeshortcut      Removes a shortcut"
-    fi
+    while getopts "hlmdn:r:" option; do
+        case $option in
+            h) # help
+                usage
+                return;;
+            l) # list all shortcuts
+                listall
+                return;;
+            m) # create container dir
+                makedir
+                return;;
+            d) # delete container dir
+                deletedir
+                return;;
+            n) # create new shortcut
+                newshortcut
+                return;;
+            r) # remove shortcut
+                removeshortcut
+                return;;
+            *) # default
+                echo "Error: Invalid option"
+                return;;
+        esac
+    done
+}
 
-    # list all shortcuts
-    if [ $1 == "-l" ] || [ $1 == "--list" ]
-    then
-        echo "All shortcuts:"
-        for x in "${!shortcuts[@]}"
-        do
-            echo "$x|${shortcuts[$x]}"
-        done
-    fi
 
-    # create container dir
-    if [ $1 == "-m" ] || [ $1 == "--makedirectory" ]
-    then
-        mkdir ~/BashCDShortcuts
-        echo "Created directory for container at ~/BachCDShortcuts"
-        touch ~/BashCDShortcuts/shortcuts.txt
-        echo "Created container for shortcuts at ~/BashCDShortcuts/shortcuts.txt"
-        fill_array
+usage() {
+    echo "options:"
+    echo "-l      Shows all available shortcuts"
+    echo "-m      Creates shortcut container directory"
+    echo "-d      Deletes shortcut container directory"
+    echo "-c      Changes directory to the value of a shortcut"
+    echo "-n      Creates a new shortcut"
+    echo '  syntax  "KEY|PATH" include double quotes and "|"'
+    echo "-r      Removes a shortcut"
+}
+
+listall() {
+    echo "All shortcuts:"
+    for x in "${!shortcuts[@]}"
+    do
+        echo "$x=${shortcuts[$x]}"
+    done
+}
+
+makedir() {
+    if [ -e ~/BashCDShortcuts ]; then
+        echo "Container dir already exists"
         return
     fi
+    mkdir ~/BashCDShortcuts
+    echo "Created directory for container at ~/BachCDShortcuts"
+    touch ~/BashCDShortcuts/shortcuts.txt
+    echo "Created container for shortcuts at ~/BashCDShortcuts/shortcuts.txt"
+    fill_array
+}
 
-    # delete container dir
-    if [ $1 == "-d" ] || [ $1 == "--delete" ] && [ -e ~/BashCDShortcuts ]
-    then
-        rm -rf ~/BashCDShortcuts
-        echo "Delete ~/BashCDShortcuts recursively"
-    fi
+deletedir() {
+    rm -rf ~/BashCDShortcuts
+    echo "Deleted ~/BashCDShortcuts recursively"
+}
 
-    # change directory to a shortcut
-    if [ $1 == "-c" ] || [ $1 == "--changedirectory" ]
-    then
-        if [ ! -z "${shortcuts["$2"]}" ]
+newshortcut() {
+    echo "$OPTARG" >> ~/BashCDShortcuts/shortcuts.txt
+    fill_array
+}
+
+removeshortcut() {
+    echo "$OPTARG=${shortcuts[$OPTARG]}"
+    # echo "removed $1 from shortcuts"
+    sed -i '/$OPTARG=${shortcuts[$OPTARG]}/d' ~/BashCDShortcuts/shortcuts.txt
+}
+
+changedir() {
+    if [ "${shortcuts[$cdarg]}" ]
         then
-            cd ~/${shortcuts[$2]}
+            cd ~/${shortcuts[$cdarg]}
             pwd
         else
             echo "Shortcut not found"
-        fi
     fi
+}
 
-    # create new shortcut
-    if [ $1 == "-n" ] || [ $1 == "--newshortcut" ]
-    then
-        echo "$2" >> ~/BashCDShortcuts/shortcuts.txt
-        fill_array
-    fi
-
+function temp() {
     # remove shortcut
     if [ $1 == "-r" ] || [ $1 == "--removeshortcut" ]
     then
